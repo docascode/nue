86cdf375c706c019db952c4504700e52c81edfe4
Den Delimarsky <ddelimarsky@live.com>
2018-05-25 17:03:33 -0700

    :fire: PowerShell Gallery support

--- Nue/Nue.Core/PackageAtom.cs
+++ Nue/Nue.Core/PackageAtom.cs
@@ -8,6 +8,7 @@ namespace Nue.Core
         public string Name { get; set; }
         public string Version { get; set; }
         public string MonikerBase { get; set; }
+        public bool IsPowerShellPackage { get; set; }
         public bool IsMetaPackage { get; set; }
         public Dictionary<string,string> CustomPropertyBag { get; set; }
         public string GetFullName()
--- Nue/Nue.StandardResolver/Resolver.cs
+++ Nue/Nue.StandardResolver/Resolver.cs
@@ -20,14 +20,18 @@ namespace Nue.StandardResolver
     {
         public IDictionary<string, string> Parameters { get; set; }
 
-        public async Task<bool> CopyBinarySet(PackageAtom package, string outputPath, KeyValuePair<string, string> credentials = new KeyValuePair<string, string>())
+        public async Task<bool> CopyBinarySet(PackageAtom package, string outputPath, KeyValuePair<string, string> credentials = new KeyValuePair<string, string>(), string feed = "")
         {
             string defaultPackageSource = "https://api.nuget.org/v3/index.json";
+            if (!string.IsNullOrWhiteSpace(feed))
+            {
+                defaultPackageSource = feed;
+            }
 
             // Check if we have a requirement for a custom package source
             if (package.CustomPropertyBag.ContainsKey("customSource"))
             {
-                defaultPackageSource =package.CustomPropertyBag["customSource"];
+                defaultPackageSource = package.CustomPropertyBag["customSource"];
             }
 
             Parameters = new Dictionary<string, string>(package.CustomPropertyBag);
@@ -57,13 +61,6 @@ namespace Nue.StandardResolver
             INuGetProjectContext projectContext = new ProjectContext();
             var sourceRepository = new SourceRepository(packageSource, providers);
 
-            // Check if we have a metapackage flag
-            if (package.CustomPropertyBag.ContainsKey("metapackage"))
-            {
-                var metaFlag = Convert.ToBoolean(package.CustomPropertyBag["metapackage"]);
-                package.IsMetaPackage = metaFlag;
-            }
-
             ConsoleEx.WriteLine($"Attempting to install: {package.GetFullName()}. Installing...",
                     ConsoleColor.Yellow);
             var identity = new PackageIdentity(package.Name, NuGetVersion.Parse(package.Version));
@@ -79,7 +76,12 @@ namespace Nue.StandardResolver
             string pacManPackageLibPath = "";
 
             // In some cases, the lookup might be happening inside a custom path.
-            if (package.CustomPropertyBag.ContainsKey("libpath") && !string.IsNullOrWhiteSpace(package.CustomPropertyBag["libpath"]))
+            // For PowerShell, this should be done inside the root directory.
+            if (package.IsPowerShellPackage)
+            {
+                pacManPackageLibPath = pacManPackagePath;
+            }
+            else if (package.CustomPropertyBag.ContainsKey("libpath") && !string.IsNullOrWhiteSpace(package.CustomPropertyBag["libpath"]))
             {
                 pacManPackageLibPath = pacManPackagePath + Convert.ToString(package.CustomPropertyBag["libpath"]);
             }
@@ -95,58 +97,92 @@ namespace Nue.StandardResolver
             // another team.
             if (Directory.Exists(pacManPackageLibPath) && !Directory.Exists(packageContainerPath))
             {
-                // Directory exists, so we should proceed to package extraction.
-                var directories = Directory.GetDirectories(pacManPackageLibPath);
-                var closestDirectory = Helpers.GetBestLibMatch(Parameters["tfm"], directories);
-
-                var availableMonikers = new List<string>();
-                var dependencyFolders = from c in Directory.GetDirectories(outputPath + "\\_pacman")
-                                        where Path.GetFileName(c).ToLower() != packageFqn.ToLower()
-                                        select c;
-
-                Console.WriteLine("Currently available lib sets:");
-                ConsoleEx.WriteLine("|__", ConsoleColor.Yellow);
-                foreach (var folder in directories)
-                {
-                    var tfmFolder = Path.GetFileName(folder);
-                    availableMonikers.Add(tfmFolder);
-                    ConsoleEx.WriteLine("   |___" + tfmFolder, ConsoleColor.Yellow);
-                }
-
-                Console.WriteLine("Package dependencies:");
-                ConsoleEx.WriteLine("|__", ConsoleColor.Yellow);
-                foreach (var dependency in dependencyFolders)
-                    ConsoleEx.WriteLine("   |___" + Path.GetFileNameWithoutExtension(dependency),
-                        ConsoleColor.Yellow);
-
-                var frameworkIsAvailable = !string.IsNullOrWhiteSpace(closestDirectory);
-
-
-                // We could not find a closest folder, so let's just check in the root.
-
-                var binaries = Directory.EnumerateFiles(pacManPackageLibPath, "*.*", SearchOption.TopDirectoryOnly)
-                                .Where(s => s.EndsWith(".dll") || s.EndsWith(".winmd"));
-                var docFiles = Directory.GetFiles(pacManPackageLibPath,
-                    "*.xml",
-                    SearchOption.TopDirectoryOnly);
-
                 Directory.CreateDirectory(packageContainerPath);
 
-                // Make sure to only go through any processing if we found binaries.
-                if (binaries.Any())
+                // If we are dealing with a different PowerShell package, we might need to operate slightly
+                // differently givent that the structure is not at all reflective of what other NuGet packages encompass.
+                if (package.IsPowerShellPackage)
                 {
-                    foreach (var binary in binaries)
-                        File.Copy(binary, Path.Combine(packageContainerPath, Path.GetFileName(binary)), true);
+                    ConsoleEx.WriteLine("Operating on a PowerShell package...", ConsoleColor.Blue);
 
-                    foreach (var docFile in docFiles)
-                        File.Copy(docFile, Path.Combine(packageContainerPath, Path.GetFileName(docFile)), true);
+                    var helpXmlFiles = from c in Directory.GetFiles(pacManPackageLibPath)
+                                       where Path.GetFileName(c).ToLower().EndsWith("-help.xml")
+                                       select c;
+
+                    var dllFiles = new List<string>();
+
+                    Console.WriteLine("Here are the XML files that we will work with:");
+                    foreach(var helpXmlFile in helpXmlFiles)
+                    {
+                        ConsoleEx.WriteLine(helpXmlFile, ConsoleColor.Blue);
+                        var workingDll = Path.GetFileName(helpXmlFile).ToLower().Replace("-help.xml", "");
+                        if (File.Exists(Path.Combine(pacManPackageLibPath, workingDll)))
+                        {
+                            ConsoleEx.Write("Found matching DLL: ");
+                            ConsoleEx.Write(workingDll + "\n", ConsoleColor.Blue);
+
+                            dllFiles.Add(workingDll);
+                        }
+                    }
+
+                    if (dllFiles.Any())
+                    {
+                        foreach (var dll in dllFiles)
+                        {
+                            File.Copy(Path.Combine(pacManPackageLibPath, dll), Path.Combine(packageContainerPath, dll), true);
+                            File.Copy(Path.Combine(pacManPackageLibPath, dll + "-help.xml"), Path.Combine(packageContainerPath, Path.GetFileNameWithoutExtension(dll) + ".xml"), true);
+                        }
+
+                        var dependencies = from c in Directory.GetFiles(pacManPackageLibPath)
+                                           where !dllFiles.Contains(Path.GetFileName(c).ToLower()) && Path.GetFileName(c).EndsWith(".dll")
+                                           select c;
+
+                        if (dependencies.Any())
+                        {
+                            Directory.CreateDirectory(Path.Combine(outputPath, "dependencies", package.Moniker));
+
+                            foreach (var dependency in dependencies)
+                            {
+
+                                File.Copy(dependency, Path.Combine(outputPath, "dependencies", package.Moniker, Path.GetFileName(dependency)), true);
+                            }
+                        }
+                    }
                 }
-
-                if (frameworkIsAvailable)
+                else
                 {
-                    binaries = Directory.EnumerateFiles(closestDirectory, "*.*", SearchOption.TopDirectoryOnly)
+                    // Directory exists, so we should proceed to package extraction.
+                    var directories = Directory.GetDirectories(pacManPackageLibPath);
+                    var closestDirectory = Helpers.GetBestLibMatch(Parameters["tfm"], directories);
+
+                    var availableMonikers = new List<string>();
+                    var dependencyFolders = from c in Directory.GetDirectories(outputPath + "\\_pacman")
+                                            where Path.GetFileName(c).ToLower() != packageFqn.ToLower()
+                                            select c;
+
+                    Console.WriteLine("Currently available lib sets:");
+                    ConsoleEx.WriteLine("|__", ConsoleColor.Yellow);
+                    foreach (var folder in directories)
+                    {
+                        var tfmFolder = Path.GetFileName(folder);
+                        availableMonikers.Add(tfmFolder);
+                        ConsoleEx.WriteLine("   |___" + tfmFolder, ConsoleColor.Yellow);
+                    }
+
+                    Console.WriteLine("Package dependencies:");
+                    ConsoleEx.WriteLine("|__", ConsoleColor.Yellow);
+                    foreach (var dependency in dependencyFolders)
+                        ConsoleEx.WriteLine("   |___" + Path.GetFileNameWithoutExtension(dependency),
+                            ConsoleColor.Yellow);
+
+                    var frameworkIsAvailable = !string.IsNullOrWhiteSpace(closestDirectory);
+
+
+                    // We could not find a closest folder, so let's just check in the root.
+
+                    var binaries = Directory.EnumerateFiles(pacManPackageLibPath, "*.*", SearchOption.TopDirectoryOnly)
                                     .Where(s => s.EndsWith(".dll") || s.EndsWith(".winmd"));
-                    docFiles = Directory.GetFiles(closestDirectory,
+                    var docFiles = Directory.GetFiles(pacManPackageLibPath,
                         "*.xml",
                         SearchOption.TopDirectoryOnly);
 
@@ -159,55 +195,74 @@ namespace Nue.StandardResolver
                         foreach (var docFile in docFiles)
                             File.Copy(docFile, Path.Combine(packageContainerPath, Path.GetFileName(docFile)), true);
                     }
-                }
 
-                foreach (var dependency in dependencyFolders)
-                {
-                    var availableDependencyMonikers = new List<string>();
-
-                    var targetPath = Path.Combine(dependency, "lib");
-                    if (Directory.Exists(targetPath) && Directory.EnumerateFiles(targetPath, "*.*", SearchOption.AllDirectories)
-                            .Where(s => s.EndsWith(".dll") || s.EndsWith(".winmd")).Count() > 0)
+                    if (frameworkIsAvailable)
                     {
-                        List<string> alternateDependencies = new List<string>();
-                        // In some cases, we might want to have alterhative dependency monikers.
-                        if (package.CustomPropertyBag.ContainsKey("altDep"))
+                        binaries = Directory.EnumerateFiles(closestDirectory, "*.*", SearchOption.TopDirectoryOnly)
+                                        .Where(s => s.EndsWith(".dll") || s.EndsWith(".winmd"));
+                        docFiles = Directory.GetFiles(closestDirectory,
+                            "*.xml",
+                            SearchOption.TopDirectoryOnly);
+
+                        // Make sure to only go through any processing if we found binaries.
+                        if (binaries.Any())
                         {
-                            alternateDependencies = new List<string>(package.CustomPropertyBag["altDep"].Split('|'));
+                            foreach (var binary in binaries)
+                                File.Copy(binary, Path.Combine(packageContainerPath, Path.GetFileName(binary)), true);
+
+                            foreach (var docFile in docFiles)
+                                File.Copy(docFile, Path.Combine(packageContainerPath, Path.GetFileName(docFile)), true);
                         }
+                    }
 
-                        var dependencyLibFolders = Directory.GetDirectories(Path.Combine(dependency, "lib"));
-                        var closestDepLibFolder = Helpers.GetBestLibMatch(Parameters["tfm"], dependencyLibFolders);
+                    foreach (var dependency in dependencyFolders)
+                    {
+                        var availableDependencyMonikers = new List<string>();
 
-                        if (string.IsNullOrWhiteSpace(closestDepLibFolder))
+                        var targetPath = Path.Combine(dependency, "lib");
+                        if (Directory.Exists(targetPath) && Directory.EnumerateFiles(targetPath, "*.*", SearchOption.AllDirectories)
+                                .Where(s => s.EndsWith(".dll") || s.EndsWith(".winmd")).Count() > 0)
                         {
-                            // We could not find a regular TFM dependency, let's try again for alternates.
-                            if (alternateDependencies.Count > 0)
+                            List<string> alternateDependencies = new List<string>();
+                            // In some cases, we might want to have alterhative dependency monikers.
+                            if (package.CustomPropertyBag.ContainsKey("altDep"))
                             {
-                                foreach (var altDependency in alternateDependencies)
+                                alternateDependencies = new List<string>(package.CustomPropertyBag["altDep"].Split('|'));
+                            }
+
+                            var dependencyLibFolders = Directory.GetDirectories(Path.Combine(dependency, "lib"));
+                            var closestDepLibFolder = Helpers.GetBestLibMatch(Parameters["tfm"], dependencyLibFolders);
+
+                            if (string.IsNullOrWhiteSpace(closestDepLibFolder))
+                            {
+                                // We could not find a regular TFM dependency, let's try again for alternates.
+                                if (alternateDependencies.Count > 0)
                                 {
-                                    closestDepLibFolder = Helpers.GetBestLibMatch(altDependency, dependencyLibFolders);
-                                    if (!string.IsNullOrWhiteSpace(closestDepLibFolder))
-                                        break;
+                                    foreach (var altDependency in alternateDependencies)
+                                    {
+                                        closestDepLibFolder = Helpers.GetBestLibMatch(altDependency, dependencyLibFolders);
+                                        if (!string.IsNullOrWhiteSpace(closestDepLibFolder))
+                                            break;
+                                    }
                                 }
                             }
-                        }
 
-                        var dFrameworkIsAvailable = !string.IsNullOrWhiteSpace(closestDepLibFolder);
+                            var dFrameworkIsAvailable = !string.IsNullOrWhiteSpace(closestDepLibFolder);
 
-                        if (dFrameworkIsAvailable)
-                        {
-                            Directory.CreateDirectory(Path.Combine(outputPath, "dependencies",
-                                package.Moniker));
+                            if (dFrameworkIsAvailable)
+                            {
+                                Directory.CreateDirectory(Path.Combine(outputPath, "dependencies",
+                                    package.Moniker));
 
-                            var dependencyBinaries = Directory.EnumerateFiles(closestDepLibFolder, "*.*", SearchOption.TopDirectoryOnly)
-                            .Where(s => s.EndsWith(".dll") || s.EndsWith(".winmd"));
+                                var dependencyBinaries = Directory.EnumerateFiles(closestDepLibFolder, "*.*", SearchOption.TopDirectoryOnly)
+                                .Where(s => s.EndsWith(".dll") || s.EndsWith(".winmd"));
 
-                            foreach (var binary in dependencyBinaries)
-                                File.Copy(binary,
-                                    Path.Combine(outputPath, "dependencies", package.Moniker,
-                                        Path.GetFileName(binary)), true);
+                                foreach (var binary in dependencyBinaries)
+                                    File.Copy(binary,
+                                        Path.Combine(outputPath, "dependencies", package.Moniker,
+                                            Path.GetFileName(binary)), true);
 
+                            }
                         }
                     }
                 }
