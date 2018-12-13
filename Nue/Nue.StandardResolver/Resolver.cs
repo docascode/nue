8927a20c06c689b5c391830d446e4aa4b2b2cb50
Tianqi Zhang <tianzh@microsoft.com>
2018-12-13 09:02:16 +0800

    only extract once for each package

--- Nue/Nue.StandardResolver/Resolver.cs
+++ Nue/Nue.StandardResolver/Resolver.cs
@@ -75,7 +75,7 @@ namespace Nue.StandardResolver
 
                 // Among other things, we need to make sure that the package was not already extracted for 
                 // another team.
-                if (Directory.Exists(pacManPackageLibPath))
+                if (Directory.Exists(pacManPackageLibPath) && !Directory.Exists(packageContainerPath))
                 {
                     Directory.CreateDirectory(packageContainerPath);
 
